1c62fa573d0ff8a7e6fc0fdf3052c8dc38839cb7
NetraSawant <rane.netra1981@gmail.com>
2025-02-17 21:33:50 -0700

    Update ElementColors.java

--- AutoFrameWork7/src/test/java/Assignments/ElementColors.java
+++ AutoFrameWork7/src/test/java/Assignments/ElementColors.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ElementColors {
-	//Hello GoodMorning	
+	//Hello GoodMorning i am Netra	
 
 	public static void main(String[] args) {
 		
